47db638909251727f569d5136d2da6584bf828f3
Dunhilss <treborg28@gmail.com>
2023-12-28 22:18:16 +0200

    second task complete

--- main.py
+++ main.py
@@ -8,3 +8,10 @@ for char in string:
         digit_count += 1
 print("Number of letters in the text:  ", letter_count)
 print("The number of digits in the text: ", digit_count)
+numwords = list(map(int,input("\nPlease enter a number to search for: ").split()))
+numdigits = int(input("Enter the number to search for: "))
+num = 0
+for i in numwords:
+    if i == numdigits:
+        num += 1
+print(f"Number {numdigits} occurs {num} times")
